b843e4148dc7ac2181c3cb9369971a8234dcc639
fdehech <81689474+fdehech@users.noreply.github.com>
2025-04-14 03:29:24 +0100

    Fixed Form Javascript

--- Frontend/Assets/js/background.js
+++ Frontend/Assets/js/background.js
@@ -32,19 +32,21 @@ setInterval(() => {
 }, 10000);
 
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
+document.addEventListener("DOMContentLoaded", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-
+  
         console.log("Latitude:", lat, "Longitude:", lon);
-
-        var map = L.map('map').setView([lat , lon], 9);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-
-      }
-    )
-};
+  
+        var map = L.map('map').setView([lat, lon], 7);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+      });
+    }
+  });
 
 
--- Frontend/Assets/js/signup.js
+++
@@ -1,8 +0,0 @@
-const iframe = document.getElementById('myIframe');
-iframe.onload = function () {
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-};
-
-function nextform(){
-    iframe.setAttribute("src","form3.html");
-}
