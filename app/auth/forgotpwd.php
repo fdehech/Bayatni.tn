2088391664acab18cafc2ec08f1af8033aabba63
fdehech <81689474+fdehech@users.noreply.github.com>
2025-04-25 20:22:26 +0100

    Last Commit probably

--- app/auth/forgotpwd.php
+++ app/auth/forgotpwd.php
@@ -88,28 +88,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   </div>
   <?php include __DIR__.'/../../includes/header.php'; ?>
   <main>
+    <!-- Message de Erreur -->
+    <?php if (!empty($errors)): ?>
+      <div id="errors">
+        <?php foreach ($errors as $error): ?>
+          <div style="position:absolute; left:42%; text-align:center;" class="p-4 mt-10 mb-4 text-sm text-red-700 bg-red-100 rounded-lg"> <?= htmlspecialchars($error) ?></div>
+        <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
+
+    <!-- Message de succès -->
+    <?php if (!empty($_SESSION['success'])): ?>
+      <div style="position:absolute; left:37.5%; text-align:center;" class="p-4 mt-10 mb-5 text-sm text-green-700 bg-green-100 rounded-lg"> <?= $_SESSION['success']; unset($_SESSION['success']); ?></div>
+    <?php endif; ?>
+
+    <!-- Formulaire de réinitialisation du mot de passe -->
+
     <section class="auth-card">
       <form class="form" method="POST" action="">
         <h1 class="fma" style="">Réinitialisation du </br> Mot de Passe</h1>
         
-        <div class="inputForm" style=""> 
-          <svg height="20" viewBox="-64 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path><path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path></svg>
+        <div class="inputForm mt-3"> 
+          <svg height="20" viewBox="-64 0 512 512" style="margin-left:10px;" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path><path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path></svg>
           <input type="email" class="input" name="email" placeholder="Entrez votre adresse e-mail" required>
         </div>
-
-        <!-- Messages d'erreur -->
-        <?php if (!empty($errors)): ?>
-          <div id="errors">
-            <?php foreach ($errors as $error): ?>
-              <div style="justify-self:center; text-align:center;" class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg"> Erreur <?= htmlspecialchars($error) ?></div>
-            <?php endforeach; ?>
-          </div>
-        <?php endif; ?>
-
-        <!-- Message de succès -->
-        <?php if (!empty($_SESSION['success'])): ?>
-          <div style="justify-self:center; text-align:center;" class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg"> Succes <?= $_SESSION['success']; unset($_SESSION['success']); ?></div>
-        <?php endif; ?>
 
         <div class="inputSubmit">
           <button class="button-submit" type="submit" name="recover">Envoyer le lien</button>
